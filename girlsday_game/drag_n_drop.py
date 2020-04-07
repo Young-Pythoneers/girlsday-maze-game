bde67bfb4601472732762da4733982d8efa08e91
Christiaan Lamers <Christiaan.Lamers@ordina.nl>
2020-04-07 15:00:29 +0200

    now only the top icon is dragged in drag_n_drop.py

--- girlsday_game/drag_n_drop.py
+++ girlsday_game/drag_n_drop.py
@@ -1,5 +1,6 @@
 import random
 import time
+from copy import deepcopy
 
 import pygame
 from pygame.sprite import Sprite
@@ -9,9 +10,9 @@ class Key(Sprite):
     def __init__(self, button):
         Sprite.__init__(self)
 
-        self.image=button[0]
+        self.image = button[0]
         self.clicked = False
-        self.rect = self.image.get_rect()
+        self.rect = deepcopy(self.image.get_rect())
         self.rect.y = button[1]
         self.rect.x = button[2]
         self.clicked = False
@@ -30,76 +31,71 @@ done = False
 
 clock = pygame.time.Clock()
 
-key_list = pygame.sprite.Group()
+key_list = []  # pygame.sprite.Group()
 
 
-up = [pygame.image.load("../images/buttons/up.png"),100, 10, 0]
+up = [pygame.image.load("../images/buttons/up.png"), 100, 10, 0]
 down = [pygame.image.load("../images/buttons/down.png"), 200, 10, 1]
-left = [pygame.image.load("../images/buttons/left.png"), 300, 10, 1]
-right = [pygame.image.load("../images/buttons/right.png"), 400, 10, 1]
+left = [pygame.image.load("../images/buttons/left.png"), 300, 10, 2]
+right = [pygame.image.load("../images/buttons/right.png"), 400, 10, 3]
 
-key_list.add(Key(up))
-key_list.add(Key(down))
-key_list.add(Key(left))
-key_list.add(Key(right))
+# key_list.add(Key(up))
+# key_list.add(Key(down))
+# key_list.add(Key(left))
+# key_list.add(Key(right))
 
-for key in key_list:
-    print(key.__dict__.keys())
-    print(key._Sprite__g)
-    print(key.image)
-    print(key.clicked)
-    print(key.rect)
-    print(key.id)
-    print()
-exit()
-
+key_list.append(Key(up))
+key_list.append(Key(down))
+key_list.append(Key(left))
+key_list.append(Key(right))
 
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            # if pos[0] > 100:
+            #     key_list.add(Key(up))
+            print("MouseUp")
+            for key in key_list:
+                key.clicked = False
+            # drag_id = 0
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             x = pos[0]
             y = pos[1]
-
             if event.button == 1:
-                for key in key_list:
-                    if key.rect.collidepoint(pos):
-                        key.clicked = True
-                        pygame.sprite.Sprite.remove(key)
-                        print(key_list)
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            # if pos[0] > 100:
-            #     key_list.add(Key(up))
-
-            for key in key_list:
-                key.clicked = False
-            drag_id = 0
-
+                for i in range(len(key_list) - 1, -1, -1):
+                    print(key_list[i].rect.x, key_list[i].rect.y)
+                    if key_list[i].rect.collidepoint(pos):
+                        key_list[i].clicked = True
+                        temp_key = key_list[i]
+                        key_list.remove(temp_key)
+                        key_list.append(temp_key)
+                        break
     for number, key in enumerate(key_list):
-
         # if clicked and no collision you can move the object
-        if key.clicked == True and len(pygame.sprite.spritecollide(key,key_list, False)) == 1:
+        if (
+            key.clicked
+        ):  # and len(pygame.sprite.spritecollide(key,key_list, False)) == 1:
             pos = pygame.mouse.get_pos()
 
-            px = pos[0] - (key.rect.width/2)
-            py = pos[1] - (key.rect.height/2)
+            px = pos[0] - (key.rect.width / 2)
+            py = pos[1] - (key.rect.height / 2)
 
             key.rect.x = px
             key.rect.y = py
 
             # if a collision is detected you should not be allowed to place the object
-            if len(pygame.sprite.spritecollide(key,key_list, False)) > 1:
-                #print(pygame.mouse.get_pressed())
-                key.rect.x <= px
-                key.rect.y <= py
-
+            # if len(pygame.sprite.spritecollide(key,key_list, False)) > 1:
+            #    #print(pygame.mouse.get_pressed())
+            #    key.rect.x <= px
+            #    key.rect.y <= py
 
     screen.fill(Black)
-    key_list.draw(screen)
-    pygame.display.flip()
-    clock.tick(60)
+    for key in key_list:
+        screen.blit(key.image, [key.rect[0], key.rect[1]])
+    pygame.display.update()
+    # clock.tick(60)
 pygame.quit()
